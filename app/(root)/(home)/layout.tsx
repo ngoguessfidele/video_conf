48d6d09378c6b17d4672d85e7344ec2e1b544c52
ngoguessfidele <ngoguessfidele@gmail.com>
2024-04-12 14:24:32 +0200

    final

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -4,12 +4,13 @@ import { Metadata } from 'next';
 import React, { ReactNode } from 'react';
 
 export const metadata: Metadata = {
-  title: "YOOM",
+  title: "VIDEO CALL",
   description: "Video calling App",
   icons: {
     icon: "/icons/logo.svg",
   },
 };
+
 
 const HomeLayout = ({children}: {children: ReactNode}) => {
   return (
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,12 +3,13 @@ import { Metadata } from "next";
 import { ReactNode } from "react";
 
 export const metadata: Metadata = {
-  title: "YOOM",
+  title: "VIDEO CALL",
   description: "Video calling App",
   icons: {
     icon: "/icons/logo.svg",
   },
 };
+
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
